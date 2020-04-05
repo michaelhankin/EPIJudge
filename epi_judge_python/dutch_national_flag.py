cb3b33fff00e50e1b26a5ce07f64024252f7d9da
Michael Hankin <mjhankin1@gmail.com>
2020-04-05 17:24:08 -0500

    solve Dutch Nat'l Flag problem

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -9,8 +9,18 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    pivot = A[pivot_index]
+    smaller, equal, larger = 0, 0, len(A)
+
+    while equal < larger:
+        if A[equal] < pivot:
+            A[equal], A[smaller] = A[smaller], A[equal]
+            equal, smaller = equal + 1, smaller + 1
+        elif A[equal] == pivot:
+            equal += 1
+        else:
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
 
 
 @enable_executor_hook
